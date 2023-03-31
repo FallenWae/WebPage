8a514669684260d0454746f615171a1ab0df2a9c
FallenWae <127284570+FallenWae@users.noreply.github.com>
2023-03-31 10:53:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from load_data import user
 import dash_bootstrap_components as dbc
 
 # meta_tags are required for the app layout to be mobile responsive
-app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets = [dbc.themes.BOOTSTRAP],assets_folder=r'assets/bWLwgP.css',
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',dbc.themes.BOOTSTRAP],
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
