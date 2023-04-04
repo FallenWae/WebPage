5fdc81a3d674b8647a11e11482326d02134621e7
FallenWae <127284570+FallenWae@users.noreply.github.com>
2023-04-04 15:18:31 +0800

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -242,7 +242,7 @@ def daterange(date1, date2):
 
 # Taking the Date between StartDate and EndDate (Datetime Format Vaildation --> User Input )
 date_format = '%Y-%m-%d'
-StartDate = '2023-03-01'
+StartDate = '2023-01-02'
 EndDate = '2023-04-03'
 #StartDate = str(input("Start From ? \n"))
 #EndDate = str(input('End In ? \n'))
